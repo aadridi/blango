470169be57361514083bafa3e6f83f6b5f6f9453
Akrame-Amine LandoKahl) <aadridi@gmail.com>
2024-02-21 22:34:14 +0000

    Finish custom template tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django import template
 from django.utils.html import format_html
+from blog.models import Post
 
 
 user_model = get_user_model()
@@ -30,4 +31,15 @@ def author_details(author, current_user=None):
 
     return format_html('{}{}{}', prefix, name, suffix)
 
+@register.simple_tag
+def row(extra_classes=""):
+  return format_html('<div class="row {}">', extra_classes)
 
+@register.simple_tag
+def end_row():
+  return format_html('</div')
+
+@register.inclusion_tag("blog/post-list.html")
+def recent_posts(post):
+    posts = Post.objects.exclude(pk=post.pk)[:5]
+    return {"title": "Recent Posts", "posts": posts}
