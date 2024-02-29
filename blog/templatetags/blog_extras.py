7adff702319be5552fcbc39036693ffe37c1f672
Akrame-Amine LandoKahl) <aadridi@gmail.com>
2024-02-29 20:15:54 +0000

    Finish crispy forms

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -36,10 +36,18 @@ def row(extra_classes=""):
   return format_html('<div class="row {}">', extra_classes)
 
 @register.simple_tag
-def end_row():
+def endrow():
   return format_html('</div')
 
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
     return {"title": "Recent Posts", "posts": posts}
+
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html('<div class="col {}">', extra_classes)
+
+@register.simple_tag
+def endcol():
+  return format_html('</div')
